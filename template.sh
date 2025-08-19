e96aec4e4fcbb261f5f3ac00e819608e1d39dbf9
Purna Chandu <chandusaradhi95@gmail.com>
2025-08-19 16:19:49 +0530

    folder structure created

---
+++ template.sh
@@ -0,0 +1,15 @@
+# creating directory 
+mkdir -p src
+mkdir -p research
+
+#creaating files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env 
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.py
+
+echo "Directory and files added"
