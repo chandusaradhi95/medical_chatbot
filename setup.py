076e75295b00469e88e4a7cb587427f5a0989d1b
Purna Chandu <chandusaradhi95@gmail.com>
2025-08-19 17:00:18 +0530

    req and setup commited

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+	name="medical_chatbot",
+	version="0.1.0",
+	author="chandu",
+	author_email="chandusaradhi95@gmail.com",
+	packages=find_packages(),
+	install_requires=[]
+)
